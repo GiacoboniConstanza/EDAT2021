a0451d6874efe9b1b1f0fa5723122f06ac62abfd
Constanza <constanza.ibarra@est.fi.uncoma.edu.ar>
2022-07-07 22:20:17 -0300

    edat

---
+++ src/escapeHouse/prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package escapeHouse;
+
+/**
+ *
+ * @author Coqui
+ */
+public class prueba {
+
+    public static void main(String[] arg) {
+        System.out.println("(☞ﾟヮﾟ)☞");
+    }
+}
